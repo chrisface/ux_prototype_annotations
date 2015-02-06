8fd44af7592ccbc2e96471054f5453d00c616762
Chris Mckenzie <chrismckenzie@notonthehighstreet.com>
2015-02-06 10:29:24 +0000

    Added Cross domain middleware

--- app.js
+++ app.js
@@ -28,6 +28,16 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+
+// Cross origin middleware
+app.use(function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+    next();
+});
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/projects', projects);
